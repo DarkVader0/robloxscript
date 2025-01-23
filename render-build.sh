c1363629a59ce4d66f2510ea874b61f011c3bbc8
DarkVader <radepantelej@gmail.com>
2025-01-23 16:57:16 +0100

    12

--- render-build.sh
+++ render-build.sh
@@ -8,26 +8,24 @@ pip install -r requirements.txt
 
 STORAGE_DIR=/opt/render/project/.render
 
-# Install Chrome version 114
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome version 114"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-  dpkg -x ./google-chrome-stable_114.0.5735.90-1_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_114.0.5735.90-1_amd64.deb
-  cd $HOME/project/src
-else
-  echo "...Using Chrome version 114 from cache"
+if [[ -d $STORAGE_DIR/chrome ]]; then
+  echo "...Removing existing Chrome and ChromeDriver"
+  rm -rf $STORAGE_DIR/chrome
 fi
 
+# Install Chrome version 114
+echo "...Downloading Chrome version 114"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
+dpkg -x ./google-chrome-stable_114.0.5735.90-1_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_114.0.5735.90-1_amd64.deb
+
 # Install ChromeDriver version 114
-if [[ ! -f $STORAGE_DIR/chrome/usr/bin/chromedriver ]]; then
-  echo "...Downloading ChromeDriver version 114"
-  wget -P ./ https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-  unzip chromedriver_linux64.zip -d $STORAGE_DIR/chrome/usr/bin
-  chmod +x $STORAGE_DIR/chrome/usr/bin/chromedriver
-  rm chromedriver_linux64.zip
-else
-  echo "...Using ChromeDriver version 114 from cache"
-fi
+echo "...Downloading ChromeDriver version 114"
+wget -P ./ https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d $STORAGE_DIR/chrome/usr/bin
+chmod +x $STORAGE_DIR/chrome/usr/bin/chromedriver
+rm chromedriver_linux64.zip
+
+cd $HOME/project/src
